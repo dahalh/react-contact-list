eabe810b8e8c3a86ca46a90baa0c91c836d0b1e6
dahalh <dahalh@outlook.com>
2022-03-21 09:26:51 +1100

    layout created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { Title } from "./components/Title";
+import { SearchFilter } from "./components/SearchFilter";
+import { ContactList } from "./components/ContactList";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        {/* title section */}
+        <Title />
+
+        {/* search and filter section */}
+        <SearchFilter />
+
+        {/* user count section */}
+        <hr />
+        <div className="row">
+          <div className="col">55 User found</div>
+        </div>
+
+        {/* contact list cards section */}
+        <ContactList />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card } from "./Card";
+
+export const ContactList = () => {
+  return (
+    <div className="row">
+      <div className="col">
+        {/* spinner */}
+        <div class="d-flex justify-content-center text-primary">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+
+        {/* cards */}
+        <Card />
+      </div>
+    </div>
+  );
+};
