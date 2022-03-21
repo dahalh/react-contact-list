870a2391b32a4c09e0a46b999234f0afbe44e9ad
dahalh <dahalh@outlook.com>
2022-03-21 13:30:48 +1100

    filter and search bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Title } from "./components/Title";
 import { SearchFilter } from "./components/SearchFilter";
 import { ContactList } from "./components/ContactList";
+import { fetchUsers } from "./helper/apiCall";
+import { Spinner } from "./components/Spinner";
 
 const App = () => {
+  const [user, setUser] = useState([]);
+  const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // fetch(apiUrl)
+    //   .then((resp) => {
+    //     return resp.json();
+    //   })
+    //   .then((data) => console.log(data));
+    setLoading(true);
+    fetchUsers().then((data) => {
+      setContacts(data.results);
+      setUser(data.results);
+      setLoading(false);
+    });
+  }, []);
+
+  const handleOnGenderChange = (e) => {
+    const { value } = e.target;
+    setLoading(true);
+    console.log(value);
+    const params = `results=20&gender=${value}`;
+    fetchUsers(params).then((data) => {
+      setContacts(data.results);
+      setUser(data.results);
+      setLoading(false);
+    });
+  };
+
+  const handleonSearch = (e) => {
+    const { value } = e.target;
+
+    // filter
+
+    const filterArgs = user.filter((usr) => {
+      const userName = (usr.name.first + " " + usr.name.last).toLowerCase();
+      if (userName.includes(value.toLowerCase())) {
+        return true;
+      }
+    });
+
+    setContacts(filterArgs);
+  };
+
   return (
     <div className="wrapper">
       <div className="container">
@@ -11,16 +59,20 @@ const App = () => {
         <Title />
 
         {/* search and filter section */}
-        <SearchFilter />
+        <SearchFilter
+          handleOnGenderChange={handleOnGenderChange}
+          handleonSearch={handleonSearch}
+        />
 
         {/* user count section */}
         <hr />
         <div className="row">
-          <div className="col">55 User found</div>
+          <div className="col">{contacts.length} User found</div>
         </div>
 
+        {loading && <Spinner />}
         {/* contact list cards section */}
-        <ContactList />
+        <ContactList users={contacts} />
       </div>
     </div>
   );
--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-export const SearchFilter = () => {
+export const SearchFilter = ({ handleOnGenderChange, handleonSearch }) => {
   return (
     <div className="row">
       <div className="col">
-        <input type="text" className="form-control" />
+        <input type="text" className="form-control" onChange={handleonSearch} />
       </div>
       <div className="col">
-        <select name="" id="" className="form-control">
+        <select
+          name=""
+          id=""
+          className="form-control"
+          onChange={handleOnGenderChange}
+        >
           <option value="">-- Select Gender --</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
